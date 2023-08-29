a7587782f5feb110575a7492823893130e319ed6
AbnerRS <abnersesisenai@gmail.com>
2023-08-29 11:25:25 -0300

    MEM - V17 - Observable.

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,5 +1,7 @@
+import { PensamentoService } from './../pensamento.service';
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento/pensamento';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,18 +11,25 @@ import { Pensamento } from '../pensamento/pensamento';
 export class CriarPensamentoComponent implements OnInit {
 
   pensamento: Pensamento= {
-    id : 1,
-    conteudo: 'Aprendendo Angular',
-    autoria: 'Dev',
-    modelo: 'modelo2'
+    conteudo: '',
+    autoria: '',
+    modelo: 'modelo1'
   }
-  constructor() { }
+  constructor(private service : PensamentoService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   criarPensamento() {
-    alert("Novo pensamento criado")
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+
+  cancelar(){
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
   }
 
 }
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Pensamento } from './pensamento/pensamento';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PensamentoService {
 
-   private readonly API = 'http://localhost:3000/pensamento'
+   private readonly API = 'http://localhost:3000/pensamentos'
 
   constructor(private http: HttpClient) { }
 
   listar(){
     return this.http.get<Pensamento[]>(this.API)
+  }
+
+  criar(pensamento: Pensamento): Observable<Pensamento>{
+    return this.http.post<Pensamento>(this.API, pensamento)
   }
 }
