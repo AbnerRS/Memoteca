3815e008f0940a9f5cd4be2fea372ad65e7a1567
AbnerRS <abnersesisenai@gmail.com>
2023-08-30 15:53:59 -0300

    MEM - V23 - Validators pattern e minLength.

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -18,8 +18,14 @@ export class CriarPensamentoComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: ['', [Validators.required]],
-      autoria: ['' , [Validators.required]],
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*S(.|\s)*/)
+      ])],
+      autoria: ['' , Validators.compose([
+        Validators.required,
+        Validators.minLength(3)
+      ])],
       modelo: ['modelo1']
     })
   }
