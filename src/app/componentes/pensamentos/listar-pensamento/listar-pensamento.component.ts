a94780e53ba645b9d96d1ec109870464523969fb
AbnerRS <abnersesisenai@gmail.com>
2023-09-01 16:31:13 -0300

    MEM - V27 - Botão para carregar mais pensamentos.

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -11,6 +11,7 @@ export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
 
   constructor(private service: PensamentoService) { }
 
@@ -20,4 +21,12 @@ export class ListarPensamentoComponent implements OnInit {
     })
   }
 
+  carregarMaisPensamentos(){
+    this.service.listar(++this.paginaAtual).subscribe(listaPensamentos => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if(!listaPensamentos.length){
+        this.haMaisPensamentos = false
+      }
+    })
+  }
 }
